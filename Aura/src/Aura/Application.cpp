a45975d114655764d1929683712bff69af05ee95
deimari <lgtbozdogan@gmail.com>
2021-07-18 01:31:28 +0300

    Input polling added [WindowsWindow files moved to an new folder(Windows)].

--- Aura/src/Aura/Application.cpp
+++ Aura/src/Aura/Application.cpp
@@ -3,6 +3,8 @@
 
 #include <glad/glad.h>
 
+#include "Input.h"
+
 namespace Aura {
 
 #define BIND_EVENT_FN(x) std::bind(&Application::x, this, std::placeholders::_1)
@@ -58,6 +60,9 @@ namespace Aura {
 			
 			for (Layer* layer : m_LayerStack)
 				layer->OnUpdate();
+
+			auto [x, y] = Input::GetMousePosition();
+			AU_CORE_TRACE("{0}, {1}", x, y);
 
 			m_Window->OnUpdate();
 		}
---
+++ Aura/src/Platform/Windows/WindowsInput.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include "Aura/Input.h"
+
+namespace Aura {
+
+	class WindowsInput : public Input
+	{
+		virtual bool IsKeyPressedImpl(int keycode) override;
+
+		virtual bool IsMouseButtonPressedImpl(int button) override;
+		virtual std::pair<float, float> GetMousePositionImpl() override;
+		virtual float GetMouseXImpl() override;
+		virtual float GetMouseYImpl() override;
+	};
+}
